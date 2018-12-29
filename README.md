c1084d27cb4e062822ab1bd2d0841f1fc62f27bb
Razhelq <mateuszszpakowski@wp.pl>
2018-12-29 19:35:46 +0100

    pdf paranoia app added

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 Useful scripts I used for job automation
 
-- video_file_finder.py - searches for videofile inside given directory
+- video_file_finder.py - searches for videofiles inside given directory
 - mapit.py - launches a map in the browser using an address from the command line or clipboard
+- pdf_paranoia.py - finds all pdf files in current working directory and encrypt them with provided password
---
+++ pdf_paranoia.py
@@ -0,0 +1,19 @@
+# pdf_paranoia.py - finds all pdf files in current working directory and encrypt them with provided password
+
+import sys, PyPDF2, os
+
+pw = sys.argv[1]
+
+for foldername, subfolders, filenames in os.walk('.'):
+    for filename in filenames:
+        if filename.endswith('.pdf'):
+            pdf_writer = PyPDF2.PdfFileWriter()
+            pdf_file_obj = open(os.path.join(os.getcwd() + '\\' + filename), 'rb')
+            pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
+            for page_num in range(1, pdf_reader.numPages):
+                page_obj = pdf_reader.getPage(page_num)
+                pdf_writer.addPage(page_obj)
+            pdf_output = open('{}_encrypted.pdf'.format(filename), 'wb')
+            pdf_writer.encrypt(pw)
+            pdf_writer.write(pdf_output)
+            pdf_output.close()
